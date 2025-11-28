ba3722dced1a6101ce9162467ec33d3d426bc2bf
Vrushali Pawar <vrushalipawar1802@gmail.com>
2025-11-28 22:17:13 +0530

    creted authentication API

---
+++ dist/controllers/authController.js
@@ -0,0 +1,64 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.logout = exports.signin = exports.signup = void 0;
+const User_1 = __importDefault(require("../models/User"));
+const generateToken_1 = __importDefault(require("../utils/generateToken"));
+const signup = async (req, res) => {
+    try {
+        const { name, email, password } = req.body;
+        const userExists = await User_1.default.findOne({ email });
+        if (userExists) {
+            return res.status(400).json({ message: 'User already exists' });
+        }
+        const user = await User_1.default.create({
+            name,
+            email,
+            password,
+        });
+        if (user) {
+            res.status(201).json({
+                _id: user._id,
+                name: user.name,
+                email: user.email,
+                token: (0, generateToken_1.default)(user._id.toString()),
+            });
+        }
+        else {
+            res.status(400).json({ message: 'Invalid user data' });
+        }
+    }
+    catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
+exports.signup = signup;
+const signin = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+        const user = await User_1.default.findOne({ email });
+        if (user && (await user.comparePassword(password))) {
+            res.json({
+                _id: user._id,
+                name: user.name,
+                email: user.email,
+                token: (0, generateToken_1.default)(user._id.toString()),
+            });
+        }
+        else {
+            res.status(401).json({ message: 'Invalid email or password' });
+        }
+    }
+    catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
+exports.signin = signin;
+const logout = (req, res) => {
+    // Since we are using JWT, logout is usually handled on the client side by removing the token.
+    // However, we can send a success message.
+    res.status(200).json({ message: 'Logged out successfully' });
+};
+exports.logout = logout;
